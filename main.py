df5d293929bd5a3b1ff871a1d7d8fefb8a11d923
Mansoor Basha Syed <83864755+mansoorsd@users.noreply.github.com>
2021-06-06 17:01:37 +0530

    Update main.py

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ COMPANY_NAME = "Tesla Inc"
 STOCK_ENDPOINT = "https://www.alphavantage.co/query"
 NEWS_ENDPOINT = "https://newsapi.org/v2/everything"
 
-STOCK_API_KEY = "U65M5WTZB2HX62MQ"
+STOCK_API_KEY = ""
 NEWS_API_KEY = ""
 TWILIO_SID = ''
 TWILIO_AUTH_TOKEN = ''
